4628c6f76605780a8b33e082c71d3448b5917999
Imran <imranmohammadimranttyyyyy@gmail.com>
2025-10-25 19:14:57 +0530

    Create thinkingdots.tsx

---
+++ src/components/thinkingdots.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./ThinkingDots.css";
+
+const ThinkingDots: React.FC = () => {
+  return (
+    <div className="thinking-dots">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  );
+};
+
+export default ThinkingDots;
